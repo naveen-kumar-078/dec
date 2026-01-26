b83147e8eb536d364f122e493597b16754f95947
naveen <naveenkumar071224@gmail.com>
2026-01-26 23:41:14 +0530

    java

--- java/hello.java
+++ java/hello.java
@@ -1,9 +1,51 @@
+import java.lang.System;
+import java.util.Scanner;
+
+
+//     public static void main(String[] args) {
+//         Scanner nk = new Scanner(System.in);
+//         int a = nk.nextInt();
+//         int b = nk.nextInt();
+//         System.err.println(a+b);
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class hello{
+//     public static void main(String[] args) {
+//     int a = 10;
+//     int b  = 20;
+//     int c = 30;
+//     System.err.println(a+b+c);
+
+// }
+// }
+
+
 class hello{
-    public static void main(String args[])
-    {
-       int a = 10;
-       int b = 20;
-       int c  = 30;
-       System.out.print(a+b+c);
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+       int  a = scan.nextInt();
+       int  b = scan.nextInt();
+        int c = scan.nextInt();
+        int d = a*b*c;
+        int e = a+b+c;
+        System.out.print(d/e);
+
+
     }
 }
