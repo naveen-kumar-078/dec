3222c5ea56b24e19e623ea96de5aabd720179247
naveen <naveenkumar071224@gmail.com>
2026-01-29 19:33:57 +0530

    oops concept

---
+++ python oops/py object and class/index.py
@@ -0,0 +1,78 @@
+# class person:
+#     def __init__(self,name,age,city,country):
+#         self.name = name
+#         self.age = age
+#         self.city = city
+#         self.country = country
+# p1 = person("navee",50,"oslo","norway")
+
+# print(p1.name)
+# print(p1.age)
+# print(p1.city)
+# print(p1.country)
+
+
+
+
+# class Person:
+#     def __init__(self):
+#         self.__salary = 50000   # private
+
+#     def show_salary(self):
+#         print(self.__salary)
+
+# p = Person()
+# p.show_salary()     # Works
+# print(p.__salary)   # ❌ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class person:
+    def __init__(self):
+        self._num= 25
+
+class  student (person):
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
